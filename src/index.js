118c6360c6c1dc8f37b7c4a691640defb790a97f
Nata <nata.ak848@gmail.com>
2021-08-30 22:17:36 +0400

    feat: edn task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let exp = str;
+        let vol = Math.floor(exp.length / 2);
+        for (let i = 0; i < vol; i++) {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                exp = exp.replace(bracketsConfig[j].join(''), '');
+            }
+        }
+        return exp.length == 0;
 }
